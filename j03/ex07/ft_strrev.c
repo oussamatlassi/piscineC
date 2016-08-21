505401a85bd5f7b1fa79a57ea07c132e6069ec1a
cfatrane <charlesedouardfatrane@gmail.com>
2016-08-21 19:58:57 +0200

    j03

--- j03/ex07/ft_strrev.c
+++ j03/ex07/ft_strrev.c
@@ -1,4 +1,23 @@
 char	*ft_strrev(char *str)
 {
+	char	tmp;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (j <= i)
+	{
+		tmp = str[j];
+		str[j] = str[i];
+		str[i] = tmp;
+		i--;
+		j++;
+	}
+	return (str);
 }
