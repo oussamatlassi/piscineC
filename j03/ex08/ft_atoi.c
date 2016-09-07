a350f28c94ce4e8ecc062f51644c91305a94a5ce
cfatrane <charlesedouardfatrane@gmail.com>
2016-09-07 14:41:17 +0200

    j03

---
+++ j03/ex08/ft_atoi.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int result;
+	int neg;
+
+	i = 0;
+	result = 0;
+	neg = 1;
+	while (str [i] == ' ' || str[i] == 't' || str[i] == 'n' || str[i] == 'r' || str[i] == 'v' || str[i] == 'f')
+	{
+		i++;
+	}	
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			neg = (-1);
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (neg * result);
+}
