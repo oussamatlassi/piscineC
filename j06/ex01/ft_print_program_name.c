013d7962b11f2b24581ff514fa1a0a1669a94d7d
cfatrane <charlesedouardfatrane@yahoo.fr>
2016-10-22 04:48:03 +0200

    j06

---
+++ j06/ex01/ft_print_program_name.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	argc = 0;
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
