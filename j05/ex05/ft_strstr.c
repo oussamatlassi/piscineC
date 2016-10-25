0656041cbff9c8cd285df245513cc0e61813d109
cfatrane <charlesedouardfatrane@yahoo.fr>
2016-10-26 01:20:55 +0200

    j05

--- j05/ex05/ft_strstr.c
+++ j05/ex05/ft_strstr.c
@@ -1,24 +1,20 @@
 char	*ft_strstr(char *str, char *to_find)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	i = 0;
 	j = 0;
 	while (str[i] != '\0')
 	{
-		while (str[i] == to_find[j])
+		while (str[i] != '\0' && to_find[j] != '\0' && str[i] == to_find[j])
 		{
 			i++;
 			j++;
-			if (to_find[j] == '\0')
-				return (to_find);
 		}
+		if (!to_find[j])
+			return (str + i - j);
 		i++;
-	}
-	if(str[i] == '\0')
-	{
-		return(NULL);
 	}
 	return (0);
 }
