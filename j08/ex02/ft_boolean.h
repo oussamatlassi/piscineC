9bc159fb2c2134b2b3b57c63d54aebe6f4dd111a
cfatrane <charlesedouardfatrane@yahoo.fr>
2016-10-27 03:43:42 +0200

    j08

---
+++ j08/ex02/ft_boolean.h
@@ -0,0 +1,14 @@
+#ifndef __FT_BOOLEAN_H__
+# define __FT_BOOLEAN_H__
+
+# include <unistd.h>
+# define EVEN(x) (x % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int t_bool;
+
+#endif
---
+++ j08/ex03/ft_abs.h
@@ -0,0 +1,6 @@
+#ifndef FT_ABS
+# define FT_ABS
+
+# define ABS(x) if (x > 0) ? x : -x
+
+#endif
